225f2a05571e22d23e8f7246b96246f6d0527f92
Marcilio Junior <marciliojrs@gmail.com>
2018-11-11 17:57:03 -0200

    TeamworkAPI impl

--- TeamworkAPI/Repository/ProjectRepository.swift
+++ TeamworkAPI/Repository/ProjectRepository.swift
@@ -1,1 +1,20 @@
+import RxSwift
+import Domain
 
+protocol ProjectRepositoryType: Domain.ProjectRepositoryType, AutoRequestable {
+    var networkMaker: NetworkRequestMakerType! { get }
+
+    //sourcery: method="get"
+    //sourcery: path="projects.json"
+    //sourcery: responseType=[TeamworkAPI.Project]
+    //sourcery: domainMapping
+    //sourcery: keyPath="projects"
+    func all() -> Observable<[Domain.Project]>
+
+    //sourcery: method="get"
+    //sourcery: path="projects/\(id)/.json"
+    //sourcery: responseType=TeamworkAPI.Project
+    //sourcery: domainMapping
+    //sourcery: keyPath="project"
+    func project(by id: String) -> Observable<Domain.Project>
+}
