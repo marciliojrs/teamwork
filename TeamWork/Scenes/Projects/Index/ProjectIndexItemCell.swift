9c39046e3ae6c6cba0990f032348c3dbbacf810a
Marcilio Junior <marciliojrs@gmail.com>
2018-11-12 09:46:23 -0200

    Project index list UI update

--- TeamWork/Scenes/Projects/Index/ProjectIndexItemCell.swift
+++ TeamWork/Scenes/Projects/Index/ProjectIndexItemCell.swift
@@ -3,6 +3,25 @@ import RxSwift
 import RxCocoa
 
 final class ProjectIndexItemCell: UITableViewCell {
+    @objc private var containerView: UIView!
+
+    private var isTouched: Bool = false {
+        didSet {
+            var transform = CGAffineTransform.identity
+            if isTouched { transform = transform.scaledBy(x: 0.96, y: 0.96) }
+            UIView.animate(
+                withDuration: 0.3,
+                delay: 0,
+                usingSpringWithDamping: 0.8,
+                initialSpringVelocity: 0,
+                options: [],
+                animations: { [containerView] in
+                    containerView?.transform = transform
+                }, completion: nil
+            )
+        }
+    }
+
     public static func registerIntoList(_ listView: ListViewType?,
                                         state: ViewState,
                                         constants: [String: Any]? = nil,
@@ -13,5 +32,20 @@ final class ProjectIndexItemCell: UITableViewCell {
                                  state: state,
                                  constants: constants ?? [:],
                                  forCellReuseIdentifier: reuseIdentifier)
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        isTouched = true
+    }
+
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesEnded(touches, with: event)
+        isTouched = false
+    }
+
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesCancelled(touches, with: event)
+        isTouched = false
     }
 }
